0582cfe62928ac4541f2b8540e3ff3dfbbe3bcc5
erferguson <ferguson.eric.r@gmail.com>
2020-10-25 15:31:10 -0500

    tested passed

--- adv.py
+++ adv.py
@@ -29,6 +29,55 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+# keep track of 'reverse' directions
+# use this to return to a room with valid moves
+backtrack = []
+reversed_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+# instantiate a set to keep track of visited rooms
+visited = set()
+
+# while there are still unvisited rooms
+while len(visited) < len(room.graph):
+
+    # initialize next move as None
+    next_move = None
+
+    # for each exxit(neighbor) in the room expressed by (n,s,e,w)
+    for exit in player.current_room.get_exits():
+        # for each exit, if its not been visited, set as the next move
+        if player.current_room.get_room_in_direction(exit) not in visited:
+            next_move = exit
+            # some rooms have multiple exits, this will break the loop
+            # upon the first non-visited exit. it does not improve moves 
+            # BUT is lightly faster runtime
+            break
+
+    # if there is a vialbe move...
+    if next_move is not None:
+
+        # append the move to the traversal_path
+        traversal_path.append(next_move)
+
+        # add the reversed direction to breadcrumb trail
+        backtrack.append(reversed_directions[next_move])
+
+        # make the move and add room to visited list
+        player.travel(next_move)
+        visited.add(player.current_room)
+
+    else:
+        # there is no valid room >> use BACKTRACK to go back & find a Room
+        # with Valid Move
+
+        # pop the last entry from backtrack
+        next_move = backtrack.pop()
+
+        # add that move to traversal path
+        traversal_path.append(next_move)
+
+        # make the move
+        player.travel(next_move)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
